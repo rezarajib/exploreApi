65635042acf798584b0d024f4fef92c380ec7d42
rezarajib <rajibreza64@gmail.com>
2023-03-24 03:20:25 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,2 @@
+const user = { id: 1 , name: 'reza', job: 'no job'};
+console.log(user);
---
+++ js/json.js
@@ -0,0 +1,3 @@
+const user = { id: 1 , name: 'reza', job: 'no job'};
+//  javascript object notation (JSON)
+console.log(user);
